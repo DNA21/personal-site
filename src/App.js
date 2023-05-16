fe1bf0f967be1f2c5387a55c1230b67841a8eb39
Daniel Galuardi <dan.galuardi@gmail.com>
2023-05-16 16:20:41 -0400

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-    return
+    return(
+        <Header />
+    )
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import {
+    Container,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Button,
+    Offcanvas,
+    OffcanvasHeader,
+    OffcanvasBody
+} from 'reactstrap'
+
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <Container>
+            <Navbar>
+                <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+                <Offcanvas isOpen={menuOpen} toggle={() => setMenuOpen(!menuOpen)}>
+                    <OffcanvasHeader className='row'>
+                        <div className='row'>
+                        <h4 className='col-10'>DJG.</h4>
+                        <button type='button' className='btn-close col my-0' onClick={() => setMenuOpen(!menuOpen)} />
+                        </div>
+                    </OffcanvasHeader>
+                    <OffcanvasBody>
+                        <ul>
+                            <li>
+                                About
+                            </li>
+                            <li>
+                                Projects
+                            </li>
+                            <li>
+                                Contact
+                            </li>
+                        </ul>
+                    </OffcanvasBody>
+                </Offcanvas>
+                <NavbarBrand>
+                    <h1>djg.</h1>
+                </NavbarBrand>
+                <Button>Contact Me</Button>
+            </Navbar>
+        </Container>
+    )
+}
+
+export default Header
